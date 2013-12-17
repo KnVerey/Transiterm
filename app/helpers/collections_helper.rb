fb9a5e06186e6e30598b8d1fd94d1c74ccdb7f0f
Katrina Verey <kn.verey@gmail.com>
2013-12-17 11:58:19 -0500

    Refactor to use model method for active collections

--- app/helpers/collections_helper.rb
+++ app/helpers/collections_helper.rb
@@ -1,12 +1,3 @@
 module CollectionsHelper
 
-	def lang_columns(collection = @collection)
-		columns = { titles: [] }
-
-		columns[:titles] << "English" if collection.english
-		columns[:titles] << "French" if collection.french
-		columns[:titles] << "Spanish" if collection.spanish
-
-		return columns
-	end
 end
