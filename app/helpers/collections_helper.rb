1d93d98c649548921cf6afa63111bbcf1301e7f1
Katrina Verey <kn.verey@gmail.com>
2013-12-17 01:45:33 -0500

    Layout work for collections show

--- app/helpers/collections_helper.rb
+++ app/helpers/collections_helper.rb
@@ -1,2 +1,13 @@
 module CollectionsHelper
+
+	def lang_columns
+		width = 6 / @collection.num_languages
+		columns = { width: width, titles: [] }
+
+		columns[:titles] << "English" if @collection.english
+		columns[:titles] << "French" if @collection.french
+		columns[:titles] << "Spanish" if @collection.spanish
+
+		return columns
+	end
 end
---
+++ spec/helpers/collections_helper_spec.rb
@@ -0,0 +1,10 @@
+require "spec_helper"
+
+describe CollectionsHelper do
+	describe "lang_columns" do
+		xit "returns array of headings/sizes" do
+			expect(helper.lang_columns.kind_of? Array).to be_true
+		end
+
+	end
+end
