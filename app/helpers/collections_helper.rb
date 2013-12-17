067043c4f9c9dd90cb609f98b3292b4ac73a20da
Katrina Verey <kn.verey@gmail.com>
2013-12-17 11:23:32 -0500

    term records now display

--- app/helpers/collections_helper.rb
+++ app/helpers/collections_helper.rb
@@ -1,12 +1,11 @@
 module CollectionsHelper
 
-	def lang_columns
-		width = 6 / @collection.num_languages
-		columns = { width: width, titles: [] }
+	def lang_columns(collection = @collection)
+		columns = { titles: [] }
 
-		columns[:titles] << "English" if @collection.english
-		columns[:titles] << "French" if @collection.french
-		columns[:titles] << "Spanish" if @collection.spanish
+		columns[:titles] << "English" if collection.english
+		columns[:titles] << "French" if collection.french
+		columns[:titles] << "Spanish" if collection.spanish
 
 		return columns
 	end
