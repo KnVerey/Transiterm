c7d55f7cf3ef36cec82ec3ca820ad547cf32601a
Katrina Verey <kn.verey@gmail.com>
2013-12-20 00:29:22 -0500

    Add constant for included fields

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -6,6 +6,7 @@ class Collection < ActiveRecord::Base
 	validate :valid_num_languages
 
   LANGUAGES = %w{english french spanish}
+  FIELDS = Collection::LANGUAGES.zip(%w{domain source context comment}).flatten.sort
 
 
 	def active_languages
