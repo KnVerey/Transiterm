cc042e6279b9736c159750f5dd4a31edd0524b96
Katrina Verey <kn.verey@gmail.com>
2013-12-20 03:14:10 -0500

    Corrected placement of db cleaner with spork

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,10 +15,6 @@ Spork.prefork do
 
   include Sorcery::TestHelpers::Rails
 
-  # Requires supporting ruby files with custom matchers and macros, etc,
-  # in spec/support/ and its subdirectories.
-  Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
-
   # Checks for pending migrations before tests are run.
   # If you are not using ActiveRecord, you can remove this line.
   ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
@@ -56,6 +52,9 @@ end
 Spork.each_run do
   # This code will be run each time you run your specs.
 
+  # Requires supporting ruby files with custom matchers and macros, etc,
+  # in spec/support/ and its subdirectories.
+  Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 end
 
 # --- Instructions ---
