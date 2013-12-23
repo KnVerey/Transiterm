03455a46bb49bc8e6d5c8c1914aca5b675675870
Katrina Verey <kn.verey@gmail.com>
2013-12-22 23:56:57 -0500

    Added line to hide l8n message in tests

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,8 @@ module Transiterm
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    config.i18n.enforce_available_locales = true
+
     config.generators do |g|
       g.fixture_replacement :factory_girl
     end
