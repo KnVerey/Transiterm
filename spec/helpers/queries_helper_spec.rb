dac7ca803c9a739c0a60dfcd0291a61b71027a5e
Katrina Verey <kn.verey@gmail.com>
2014-01-16 15:39:48 -0500

    Wrote helper specs for queries controller

--- spec/helpers/queries_helper_spec.rb
+++ spec/helpers/queries_helper_spec.rb
@@ -1,5 +1,79 @@
 require 'spec_helper'
 
 describe QueriesHelper do
+	let(:person) { FactoryGirl.create(:user) }
+	let(:c) { FactoryGirl.create(:collection, user: person) }
+
+	before(:each) { helper.stub(:current_user).and_return(person) }
+
+	describe "#set_columns" do
+		it "returns an array (of active langs)" do
+			expect(helper.set_columns).to be_an(Array)
+		end
+
+		it "returns array with length between 0 and all langs" do
+			max_possible = Collection::LANGUAGES.length
+
+			expect(0..max_possible).to include(helper.set_columns.length)
+		end
+	end
+
+	describe "#set_fields" do
+		it "returns an array (of fields)" do
+		  expect(helper.set_fields).to be_an(Array)
+		end
+
+		it "returns array with length btw 0 and all langs + all fields" do
+		  max_possible = Collection::LANGUAGES.length + Collection::FIELDS.length
+
+		  expect(0..max_possible).to include(helper.set_fields.length)
+		end
+	end
+
+	describe "#collection_active?" do
+		it "returns a string if the collection is active by user" do
+			person.active_collection_ids << c.id
+			helper.instance_variable_set(:@selected_collections, [c])
+
+		  expect(helper.collection_active?(c)).to be_a(String)
+		end
+
+		it "returns nil if collection is active by default (none selected)" do
+			person.active_collection_ids = []
+			helper.instance_variable_set(:@selected_collections, [c])
+
+		  expect(helper.collection_active?(c)).to be_nil
+		end
+
+		it "returns nil if collection not active" do
+			d = FactoryGirl.create(:collection, user: person)
+			person.active_collection_ids << d.id
+			helper.instance_variable_set(:@selected_collections, [d])
+
+		  expect(helper.collection_active?(c)).to be_nil
+		end
+	end
+
+	describe "#all_active?" do
+		it "returns a string if all active by default" do
+			person.active_collection_ids = []
+			helper.instance_variable_set(:@selected_collections, [c])
+
+		  expect(helper.all_active?).to be_a(String)
+		end
+
+		it "returns nil if all active by user" do
+			person.active_collection_ids << c.id
+			helper.instance_variable_set(:@selected_collections, [c])
+
+		  expect(helper.all_active?).to be_nil
+		end
+	end
+
+	describe "#format_active_langs" do
+		it "returns a string" do
+		  expect(helper.format_active_langs).to be_a(String)
+		end
+	end
 
 end
