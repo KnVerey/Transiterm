e6cc8f822b45623afeec9e66151cb5a22b0160c7
Katrina Verey <kn.verey@gmail.com>
2014-01-15 21:53:53 -0500

    Link sidebar collections to collection toggle

--- app/helpers/queries_helper.rb
+++ app/helpers/queries_helper.rb
@@ -6,4 +6,8 @@ module QueriesHelper
 	def set_fields
 		(current_user.active_languages + Collection::FIELDS).sort.map(&:capitalize)
 	end
+
+	def insert_activity_class(collection)
+		@selected_collections.include?(collection) ? "active" : "inactive"
+	end
 end
