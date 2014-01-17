4be4ec04a466bdfdc8402a3116a7baa1b16a67e5
Katrina Verey <kn.verey@gmail.com>
2014-01-16 20:55:20 -0500

    Move query service functions to module

--- app/controllers/queries_controller.rb
+++ app/controllers/queries_controller.rb
@@ -1,59 +1,12 @@
+require 'queries_service.rb'
+include QueriesService
+
 class QueriesController < ApplicationController
-
 	def show
-		@sidebar_collections = find_collections_for_sidebar
+		@sidebar_collections = find_collections_by_langs_active
 
-		@selected_collections = find_collections_for_search
-		@term_records = find_term_record_matches
+		@selected_collections = filter_collections_by_active_ids(@sidebar_collections)
+
+		@term_records = find_term_record_matches(@selected_collections)
 	end
-
-
-	private
-
-	def find_collections_for_sidebar
-		return [] if current_user.collections.count == 0
-
-		solr_query = Collection.search do
-			all_of do
-				with(:user_id, current_user.id)
-				with(:french, current_user.french_active)
-				with(:english, current_user.english_active)
-				with(:spanish, current_user.spanish_active)
-			end
-		end
-		solr_query.results
-	end
-
-	def find_collections_for_search
-		if current_user.active_collection_ids.empty?
-			@sidebar_collections
-		else
-			@sidebar_collections.select do |collection|
-				current_user.active_collection_ids.include?(collection.id)
-			end
-		end
-
-	end
-
-	def sanitize_search_field
-		((Collection::LANGUAGES + Collection::FIELDS).include?(params[:field]) && params[:field] != "All") ? params[:field].downcase : nil
-	end
-
-	def find_term_record_matches
-		return [] if @selected_collections.empty?
-
-		search_field = sanitize_search_field
-		sel_collection_ids = @selected_collections.map { |c| c.id }
-
-		solr_query = TermRecord.search do
-			keywords (params[:search] || "*"), fields: search_field
-
-			all_of do
-				with(:collection_id, sel_collection_ids)
-			end
-		end
-
-		solr_query.results
-	end
-
 end
---
+++ lib/queries_service.rb
@@ -0,0 +1,52 @@
+module QueriesService
+
+	def find_collections_by_langs_active
+		return [] if current_user.collections.count == 0
+
+		solr_query = Collection.search do
+			all_of do
+				with(:user_id, current_user.id)
+				with(:french, current_user.french_active)
+				with(:english, current_user.english_active)
+				with(:spanish, current_user.spanish_active)
+			end
+		end
+		solr_query.results
+	end
+
+	def filter_collections_by_active_ids(collection_set)
+		if no_active_ids?
+			collection_set
+		else
+			collection_set.select do |collection|
+				current_user.active_collection_ids.include?(collection.id)
+			end
+		end
+	end
+
+	def find_term_record_matches(collections)
+		return [] if collections.empty?
+
+		search_field = sanitize_search_field
+		sel_collection_ids = collections.map { |c| c.id }
+
+		solr_query = TermRecord.search do
+			keywords (params[:search] || "*"), fields: search_field
+
+			all_of do
+				with(:collection_id, sel_collection_ids)
+			end
+		end
+
+		solr_query.results
+	end
+
+	private
+	def no_active_ids?
+		current_user.active_collection_ids.empty?
+	end
+
+	def sanitize_search_field
+		((Collection::LANGUAGES + Collection::FIELDS).include?(params[:field]) && params[:field] != "All") ? params[:field].downcase : nil
+	end
+end
