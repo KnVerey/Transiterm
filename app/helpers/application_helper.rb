7fd1f7a7ffca1040946f2045b1d810445d0e341b
Katrina Verey <kn.verey@gmail.com>
2014-01-19 19:28:20 -0500

    Add markdown renderer and adjust formatting from output

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,24 @@ module ApplicationHelper
 	def format_active_langs
 		current_user.active_languages.map{ |l| l.capitalize }.join("-")
 	end
+
+	def render_markdown(content)
+		renderer = Redcarpet::Render::HTML.new(render_options = {
+		    no_images: true,
+		    hard_wrap: true,
+		    safe_links_only: true
+		  })
+
+		options = {
+			autolink: true,
+	    strikethrough: true,
+	    superscript: true,
+	    underline: true,
+	    highlight: true,
+	    space_after_headers: true
+		  }
+
+	  markdown = Redcarpet::Markdown.new(renderer, options)
+	  markdown.render(content).html_safe
+	end
 end
