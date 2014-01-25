e5f54623f35c587fcae9e2e23321df51b0836bb0
Katrina Verey <kn.verey@gmail.com>
2014-01-24 22:38:25 -0500

    Create full text search factory

---
+++ spec/factories/full_text_searches.rb
@@ -0,0 +1,17 @@
+FactoryGirl.define do
+	factory :full_text_search do
+		ignore do
+			collections []
+		end
+
+		initialize_with { new(collections) }
+
+		after(:build) do |full_text_search|
+			2.times { full_text_search.collections << FactoryGirl.build(:collection) }
+		end
+	end
+
+	factory :complete_full_text_search do
+		initialize_with { new(collections, { keywords: "fennec fox", field: "english" }) }
+	end
+end
