2b461149afbfe07372d34b57b4372af38602c002
Katrina Verey <kn.verey@gmail.com>
2014-01-26 23:07:46 -0500

    Fix and refactor new sorting.

--- app/models/full_text_search.rb
+++ app/models/full_text_search.rb
@@ -8,18 +8,8 @@ class FullTextSearch
 	end
 
 	def results
-		if @keywords.present? # leave sorted by relevance if user entered keywords
-			sunspot.results
-
-		elsif Collection::LANGUAGES.include?(@field) # sort by field name if lang field selected
-			sunspot.results.sort_by { |r| r.send(@field).downcase.gsub(/\W/, "") }
-
-		elsif Collection::FIELDS.include?(@field) # sort (differently) by field name if non-lang field selected
-			sunspot.results.sort_by { |r| r.send(@field).name.downcase.gsub(/\W/, "") }
-
-		else # sort by English if no keywords and "All" field selected
-			sunspot.results.sort_by { |r| r.english.downcase.gsub(/\W/, "") }
-		end
+		# leave sorted by relevance if user entered keywords
+		@keywords.present? ? sunspot.results : sort_results(sunspot.results)
 	end
 
 	def sunspot
@@ -38,5 +28,19 @@ class FullTextSearch
 		return nil unless field
 
 		field.downcase if (Collection::LANGUAGES + Collection::FIELDS).include?(field.downcase) && field != "All"
+	end
+
+	def sort_results(results)
+		if @field.present?
+			results.sort_by { |r| field_value(r).downcase.gsub(/\W/, "") }
+
+		else # i.e. default sort if "All" field selected
+			results.sort_by { |r| r.english.downcase.gsub(/\W/, "") }
+		end
+	end
+
+	def field_value(record) #because domain and source names are in lookup table
+		field = record.send(@field)
+		field.try(:name) ? field.name : field
 	end
 end
