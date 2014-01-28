1c564fba5d922c3c24c74ba823fce2c510005083
Katrina Verey <kn.verey@gmail.com>
2014-01-27 19:03:48 -0500

    Make term records destroy sources and domains they orphaned on destroy.

--- app/models/term_record.rb
+++ app/models/term_record.rb
@@ -8,6 +8,8 @@ class TermRecord < ActiveRecord::Base
 
 	validate :correct_languages_present
 
+	after_destroy :destroy_lookup_orphans
+
 	searchable do
 		text :english, boost: 5.0
 		text :french, boost: 5.0
@@ -27,5 +29,18 @@ class TermRecord < ActiveRecord::Base
 		end
 
 		errors.add(:base, "Please fill in all language fields") if result
+	end
+
+	def destroy_lookup_orphans
+		Source.find(self.source_id).destroy if source_orphaned?
+		Domain.find(self.domain_id).destroy if domain_orphaned?
+	end
+
+	def source_orphaned?
+		TermRecord.where(source_id: self.source_id).limit(1).empty?
+	end
+
+	def domain_orphaned?
+		TermRecord.where(domain_id: self.domain_id).limit(1).empty?
 	end
 end
--- spec/models/term_record_spec.rb
+++ spec/models/term_record_spec.rb
@@ -25,4 +25,35 @@ describe TermRecord do
   	term_record.errors.count.should == 1
   end
 
+  it "destroys orphaned sources after destroy" do
+    source = FactoryGirl.create(:source)
+    term_record = FactoryGirl.create(:term_record, source: source)
+
+    expect { term_record.destroy }.to change(Source, :count).by(-1)
+    expect { Source.find(source.id) }.to raise_error(ActiveRecord::RecordNotFound)
+  end
+
+  it "does not destroy sources that aren't orphans" do
+    source = FactoryGirl.create(:source)
+    term_record = FactoryGirl.create(:term_record, source: source)
+    successor = FactoryGirl.create(:term_record, source: source)
+
+    expect { term_record.destroy }.not_to change(Source, :count)
+  end
+
+  it "destroys orphaned domains after destroy" do
+    domain = FactoryGirl.create(:domain)
+    term_record = FactoryGirl.create(:term_record, domain: domain)
+
+    expect { term_record.destroy }.to change(Domain, :count).by(-1)
+    expect { Domain.find(domain.id) }.to raise_error(ActiveRecord::RecordNotFound)
+  end
+
+  it "does not destroy domains that aren't orphans" do
+    domain = FactoryGirl.create(:domain)
+    term_record = FactoryGirl.create(:term_record, domain: domain)
+    successor = FactoryGirl.create(:term_record, domain: domain)
+
+    expect { term_record.destroy }.not_to change(Domain, :count)
+  end
 end
