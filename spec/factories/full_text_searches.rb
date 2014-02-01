3cc385ee61df292387dbda9f79371506f5a45e95
Katrina Verey <kn.verey@gmail.com>
2014-01-31 19:21:54 -0500

    Field to 'All' in basic factory. New factories for invalid and empty searches. Add page to complete search.

--- spec/factories/full_text_searches.rb
+++ spec/factories/full_text_searches.rb
@@ -4,10 +4,18 @@ FactoryGirl.define do
 			collections []
 		end
 
-		initialize_with { new([FactoryGirl.create(:collection), FactoryGirl.create(:collection)]) }
+		initialize_with { new(collections: [FactoryGirl.create(:collection), FactoryGirl.create(:collection)], field: "All") }
 
 		factory :complete_full_text_search do
-			initialize_with { new([FactoryGirl.create(:collection), FactoryGirl.create(:collection)], { keywords: "fennec fox", field: "english" }) }
+			initialize_with { new(collections: [FactoryGirl.create(:collection), FactoryGirl.create(:collection)], keywords: "fennec fox", field: "english", page: 1) }
+		end
+
+		factory :empty_search do
+			initialize_with { new() }
+		end
+
+		factory :invalid_search do
+			initialize_with { new(field: "camel") }
 		end
 	end
 end
