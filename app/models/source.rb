237ba3cb565a0d6b1f34ed8ceb46bfbdd5b606f6
Katrina Verey <kn.verey@gmail.com>
2014-02-07 16:27:00 -0500

    Set clean name before saving source

--- app/models/source.rb
+++ app/models/source.rb
@@ -7,6 +7,8 @@ class Source < ActiveRecord::Base
 	validates :name, :user_id, presence: true
 	validates :name, uniqueness: { scope: :user_id }
 
+	before_save :set_clean_name
+
 	def self.destroy_if_orphaned(id)
 		source = Source.find(id)
 		source.destroy if source.orphaned?
@@ -18,5 +20,13 @@ class Source < ActiveRecord::Base
 
 	def orphaned?
 		self.term_records.empty?
+	end
+
+	def set_clean_name
+		self.clean_name = sanitize(name) if name_changed?
+	end
+
+	def sanitize(string)
+		ActionView::Base.full_sanitizer.sanitize(string.downcase).gsub(/[^\s\w]|_/, "")
 	end
 end
--- spec/models/source_spec.rb
+++ spec/models/source_spec.rb
@@ -10,4 +10,19 @@ describe Source do
 		source = Source.new(name: "Medical")
 		expect(source).to be_invalid
 	end
+
+	context "before save" do
+		it "populates the clean name field if it was nil" do
+			source = FactoryGirl.build(:source)
+			source.save
+			expect(source.clean_name).not_to be_nil
+		end
+
+		it "updates the clean name field if it was not nil" do
+			source = FactoryGirl.create(:source)
+			source.name = "Changed"
+			source.save
+			expect(source.clean_name).to eq("changed")
+		end
+	end
 end
