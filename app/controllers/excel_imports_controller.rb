3aeb4c24d73f4d7549cebc7c797c23761406c149
Katrina Verey <kn.verey@gmail.com>
2014-02-16 17:52:27 -0500

    Validate presence of file, use to redirect to new if nothing uploaded.

--- app/controllers/excel_imports_controller.rb
+++ app/controllers/excel_imports_controller.rb
@@ -2,4 +2,17 @@ class ExcelImportsController < ApplicationController
 	def new
 		@importer = ExcelImport.new
 	end
+
+	def create
+		@importer = ExcelImport.new(file: excel_file_param)
+		if @importer.valid?
+		else
+			render action: 'new'
+		end
+	end
+
+	private
+	def excel_file_param
+		params.try(:excel_import).try(:excel_file)
+	end
 end
--- app/models/excel_import.rb
+++ app/models/excel_import.rb
@@ -1,7 +1,17 @@
 class ExcelImport
   extend ActiveModel::Naming
 	include ActiveModel::Conversion
+	include ActiveModel::Validations
+
+	validates :file, presence: { message: "must be selected using the 'Choose File' button" }
+
+	attr_reader :file
+
 	def persisted?
 	  false
+  end
+
+  def initialize(file: nil)
+  	@file = file
   end
 end
