0fbf8d5f5f1b938fa072847dcba5234d87d07dbc
Katrina Verey <kn.verey@gmail.com>
2014-02-17 16:04:20 -0500

    Spec framework for excel imports

--- spec/factories/excel_imports.rb
+++ spec/factories/excel_imports.rb
@@ -1,6 +1,18 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
 FactoryGirl.define do
   factory :excel_import do
+
+  	initialize_with { new() }
+
+  	factory :valid_excel_import do
+	    initialize_with { new(user: FactoryGirl.create(:user), file: ActionDispatch::Http::UploadedFile.new(:tempfile => File.new("#{Rails.root}/spec/fixtures/valid_import.xls"), :filename => "valid_import.xls")) }
+  	end
+
+  	factory :invalid_excel_import do
+	    initialize_with { new(user: FactoryGirl.create(:user), file: ActionDispatch::Http::UploadedFile.new(:tempfile => File.new("#{Rails.root}/spec/fixtures/invalid_import.xls"), :filename => "invalid_import.xls")) }
+  	end
+
+  	factory :invalid_file_type_excel_import do
+	    initialize_with { new(user: FactoryGirl.create(:user), file: ActionDispatch::Http::UploadedFile.new(:tempfile => File.new("#{Rails.root}/spec/fixtures/invalid_import.xlsx"), :filename => "invalid_import.xlsx")) }
+  	end
   end
 end
--- spec/models/excel_import_spec.rb
+++ spec/models/excel_import_spec.rb
@@ -2,4 +2,16 @@ require 'spec_helper'
 
 describe ExcelImport do
 
+	it "validates that a file has been uploaded" do
+		no_file = FactoryGirl.build(:excel_import)
+		expect(no_file.valid?).to be_false
+		expect(no_file.errors.full_messages).to include("File must be selected using the 'Choose File' button")
+	end
+
+	it "validates that the file has the correct extension" do
+		invalid_extension = FactoryGirl.build(:invalid_file_type_excel_import)
+		expect(invalid_extension.valid?).to be_false
+		expect(invalid_extension.errors.full_messages).to include("File must be an Excel file (.xls) - .xlsx is not currently supported")
+	end
+
 end
