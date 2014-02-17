77092bf19cb99e18226d89f48740e840fec730fd
Katrina Verey <kn.verey@gmail.com>
2014-02-17 17:34:48 -0500

    Finish specs for create

--- spec/controllers/excel_imports_controller_spec.rb
+++ spec/controllers/excel_imports_controller_spec.rb
@@ -16,7 +16,7 @@ describe ExcelImportsController do
 			expect(assigns(:importer)).to be_an(ExcelImport)
 		end
 
-		it "respondes with 200 status" do
+		it "responds with 200 status" do
 		  login_user(person)
 		  get :new
 		  expect(response.status).to eq(200)
@@ -30,10 +30,80 @@ describe ExcelImportsController do
 		end
 
 		it "assigns a new excel import as @importer with current_user as its user" do
+			login_user(person)
 			controller.stub(:current_user).and_return(person)
 		  post :create
 		  expect(assigns(:importer)).to be_an(ExcelImport)
 		  expect(assigns(:importer).user).to eq(person)
+		end
+
+		context "when the importer is invalid (i.e. invalid file)" do
+			it "re-renders the 'new' template" do
+				login_user(person)
+				invalid_importer = FactoryGirl.build(:excel_import)
+				ExcelImport.stub(:new).and_return(invalid_importer)
+				ExcelImport.any_instance.stub(:valid?).and_return(false)
+
+				post :create
+				expect(response).to render_template("new")
+			end
+		end
+
+		context "when the importer is valid (i.e. valid file)" do
+			before(:each) { login_user(person) }
+			before(:each) { ExcelImport.any_instance.stub(:valid?).and_return(true) }
+
+			it "renders the 'show' template" do
+				valid_import = FactoryGirl.build(:valid_excel_import)
+				ExcelImport.stub(:new).and_return(valid_import)
+
+				post :create
+				expect(response).to render_template("show")
+			end
+
+			it "attempts to save records from the file" do
+				valid_import = FactoryGirl.build(:valid_excel_import)
+				ExcelImport.stub(:new).and_return(valid_import)
+
+				expect(valid_import).to receive(:save_records)
+				post :create
+			end
+
+			it "assigns @saved_records, which returns term records if import succeeded" do
+				valid_import = FactoryGirl.build(:valid_excel_import)
+				ExcelImport.stub(:new).and_return(valid_import)
+
+				post :create
+				expect(assigns(:saved_records)).to be_an(ActiveRecord::Relation)
+				expect(assigns(:saved_records)[0]).to be_a(TermRecord)
+			end
+
+			it "assigns @saved_records, which is empty if the import failed" do
+				invalid_import = FactoryGirl.build(:invalid_excel_import)
+				ExcelImport.stub(:new).and_return(invalid_import)
+
+				post :create
+				expect(assigns(:saved_records)).to be_an(ActiveRecord::Relation)
+				expect(assigns(:saved_records)).to be_empty
+			end
+
+			it "assigns @failed records, which is an empty array if import succeeded" do
+				valid_import = FactoryGirl.build(:valid_excel_import)
+				ExcelImport.stub(:new).and_return(valid_import)
+
+				post :create
+				expect(assigns(:failed_records)).to be_an(Array)
+				expect(assigns(:failed_records)).to be_empty
+			end
+
+			it "assigns @failed records, which is an array of term records if import failed" do
+				invalid_import = FactoryGirl.build(:invalid_excel_import)
+				ExcelImport.stub(:new).and_return(invalid_import)
+
+				post :create
+				expect(assigns(:failed_records)).to be_an(Array)
+				expect(assigns(:failed_records)[0]).to be_a(TermRecord)
+			end
 		end
 	end
 end
