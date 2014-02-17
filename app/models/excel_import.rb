4a0844cea9d8d5a081d79c57ba850fc6a2136120
Katrina Verey <kn.verey@gmail.com>
2014-02-17 18:57:14 -0500

    Second half of refactor

--- app/models/excel_import.rb
+++ app/models/excel_import.rb
@@ -23,21 +23,30 @@ class ExcelImport
   	@headings_map = map_headings
   	@collection = initialize_import_collection
 
-  	records_to_import = []
-
-  	@excel.each 1 do |row|
-  		break unless row.any?
-  		r = TermRecord.new(get_attributes(row))
-  		r.user = @user
-  		r.collection = @collection
-  		records_to_import << r
-  	end
-  	try_persist_records(records_to_import)
+  	temp_records = build_records_from_excel
+  	try_to_save_records(temp_records)
   end
 
   private
   def correct_file_extension
   	(errors.add(:file, "must be an Excel file (.xls) - .xlsx is not currently supported") unless @file.original_filename.match(/\.xls\z/)) if @file
+  end
+
+  def build_records_from_excel
+  	temp_records_array = []
+  	@excel.each 1 do |row|
+  		break unless row.any?
+  		temp_records_array << TermRecord.new(get_attributes(row))
+  	end
+  	temp_records_array
+  end
+
+  def get_attributes(row)
+  	attributes = {}
+  	attributes[:user] = @user
+  	attributes[:collection] = @collection
+  	@headings_map.each_key { |field| attributes[field] = row[@headings_map[field]] }
+  	attributes
   end
 
   def initialize_import_collection
@@ -53,32 +62,25 @@ class ExcelImport
 		@headings_map.has_key?(lang.to_sym) && model_row[@headings_map[lang.to_sym]].present?
   end
 
-  def try_persist_records(records_array)
+  def try_to_save_records(temp_records_array)
   	@failed_records = []
   	TermRecord.transaction do
-	  	records_array.each do |r|
-				r.save
-				@failed_records << r unless r.persisted?
+	  	temp_records_array.each do |r|
+				@failed_records << r unless r.save
 				break if @failed_records.count >= 5
 	  	end
 	  	raise ActiveRecord::Rollback if @failed_records.present?
 	  end
+	  @failed_records.empty?
   end
 
   def map_headings
   	headings_row = @excel.row(0)
-  	headings_row.inject({}) do |hash, cell|
+  	headings_row.inject({}) do |headings_hash, cell|
   		if cell.match(/\A(english|french|spanish|context|comment|domain name|source name)\z/i)
-  			hash[cell.downcase.gsub(" ", "_").to_sym] = headings_row.find_index(cell)
+  			headings_hash[cell.downcase.gsub(" ", "_").to_sym] = headings_row.find_index(cell)
   		end
-  		hash
-  	end
-  end
-
-  def get_attributes(row)
-  	attributes = @headings_map.keys.inject({}) do |attributes, field|
-  		attributes[field] = row[@headings_map[field]]
-  		attributes
+  		headings_hash
   	end
   end
 end
