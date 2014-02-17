08f2a3864a15af479e6a8557f635383c6bb01fb7
Katrina Verey <kn.verey@gmail.com>
2014-02-17 16:58:58 -0500

    Specs for excel import NEW and started for CREATE

--- spec/controllers/excel_imports_controller_spec.rb
+++ spec/controllers/excel_imports_controller_spec.rb
@@ -2,4 +2,38 @@ require 'spec_helper'
 
 describe ExcelImportsController do
 
+	let(:person) { FactoryGirl.create(:active_user) }
+
+	describe "GET new" do
+		it "redirects if user not logged in" do
+		  get :new
+		  expect(response).to redirect_to("/login")
+		end
+
+		it "assigns a new excel import as @importer" do
+			login_user(person)
+		  get :new
+			expect(assigns(:importer)).to be_an(ExcelImport)
+		end
+
+		it "respondes with 200 status" do
+		  login_user(person)
+		  get :new
+		  expect(response.status).to eq(200)
+		end
+	end
+
+	describe "POST create" do
+		it "redirects if user not logged in" do
+		  post :create
+		  expect(response).to redirect_to("/login")
+		end
+
+		it "assigns a new excel import as @importer with current_user as its user" do
+			controller.stub(:current_user).and_return(person)
+		  post :create
+		  expect(assigns(:importer)).to be_an(ExcelImport)
+		  expect(assigns(:importer).user).to eq(person)
+		end
+	end
 end
